f849a583c9760d1fc3e5af7bb28fcd6ffcdf6cb8
Joel.s.almeida <joel.almeida.sa@gmail.com>
2021-07-14 16:03:24 -0300

    main

--- app/script.js
+++ app/script.js
@@ -12,6 +12,12 @@ let mobileMenu = document.querySelector(".mobile-menu");
 
 hamburgerMenu.addEventListener('click', function () {
     mobileMenu.classList.toggle("mobile-menu--active");
+
+    if (mobileMenu.classList.contains("mobile-menu--active")) {
+        hamburgerMenuImg.src = "/images/icon-close.svg";
+    } else {
+        hamburgerMenuImg.src = "/images/icon-hamburger.svg";
+    }
 });
 
 // HAMBURGER-MENU LIST ITENS TOGGLE
